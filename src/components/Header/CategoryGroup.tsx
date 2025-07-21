2026909fbfa41bc3c71931376972500cbd8abbf5
hungmec614 <nqhung2425@gmail.com>
2025-07-21 09:41:45 +0000

    add hover category base

--- src/components/Header/CategoryGroup.tsx
+++ src/components/Header/CategoryGroup.tsx
@@ -1,14 +1,23 @@
+import React from 'react';
 import { useTranslation } from 'react-i18next';
+import CategoryList from './CategoryList';
 
 const CategoryGroup = () => {
 	const { t } = useTranslation();
+	const [isHovered, setIsHovered] = React.useState(false);
+
 	return (
-		<div className="flex bg-promotion-blue-end items-center px-4 py-3 gap-4 rounded-md">
+		<div
+			className="flex bg-promotion-blue-end items-center px-4 py-3 gap-4 rounded-md relative"
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
 			<button className="flex items-center gap-2 hover:cursor-pointer">
 				<img src="/icons/menu.svg" className="w-5 h-5" />
 				<span className="text-white text-base font-semibold leading-5">{t('header.menu.category')}</span>
 				<img src="/icons/chevron-down.svg" className="ml-4 w-5 h-5" />
 			</button>
+			<CategoryList isHovered={isHovered} />
 		</div>
 	);
 };
