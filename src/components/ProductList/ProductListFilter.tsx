1699c0de74f5c11105d0ef17235f40e5f7183b6e
hungmec614 <nqhung2425@gmail.com>
2025-07-21 07:43:37 +0000

    remove unused import

--- src/components/ProductList/ProductListFilter.tsx
+++ src/components/ProductList/ProductListFilter.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ProductListFilter = () => {
 	return <div>ProductListFilter</div>;
 };
--- src/components/PromotionBanner.tsx
+++ src/components/PromotionBanner.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const PromotionBanner = () => {
 	return <div>PromotionBanner</div>;
 };
