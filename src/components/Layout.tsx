877cddc0e2e97639b99f8b3ac675c3a933bfeddd
hungmec614 <nqhung2425@gmail.com>
2025-07-21 09:54:11 +0000

    fix css responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
 		<div className="min-h-screen flex flex-col">
 			<NewbiePromotion />
-			<div className="m-auto xl:w-[75%] flex-1 flex flex-col text-ssm">
+			<div className="m-auto w-[90%] 2xl:w-[75%] flex-1 flex flex-col text-ssm">
 				<Header />
 				<main className="flex-1 bg-red-300">{children}</main>
 				<Footer />
