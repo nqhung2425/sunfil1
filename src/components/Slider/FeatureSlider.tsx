ba7b03c5dafbf69ef5d109a75e7ebb662adcae1c
hungmec614 <nqhung2425@gmail.com>
2025-07-22 10:31:06 +0000

    fix css import

--- src/components/Slider/FeatureSlider.tsx
+++ src/components/Slider/FeatureSlider.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // import 'swiper/css';
 // import 'swiper/css/navigation';
 // import 'swiper/css/pagination';
+
 import { Product } from '../../types/Product';
 import FeatureItem from './FeatureItem';
 
