505943121868b93ba68311ab6db8b9df5f0f9834
hungmec614 <nqhung2425@gmail.com>
2025-07-22 09:23:56 +0000

    add systembox and highlights

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,8 @@ import React, { ReactNode } from 'react';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import NewbiePromotion from './NewbiePromotion';
+import ServiceHighlights from './Footer/ServiceHighlights';
+import StoreSystemBox from './StoreSystemBox';
 
 type LayoutProps = {
 	children: ReactNode;
@@ -19,6 +21,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 			</div>
 			<div className="m-auto w-[90%] 2xl:w-[75%] flex-1 flex flex-col text-ssm bg-transparent">
 				<main className="flex-1">{children}</main>
+				<ServiceHighlights />
+			</div>
+			<div className="bg-brand-blue-50">
+				<StoreSystemBox />
+			</div>
+			<div className="m-auto w-[90%] 2xl:w-[75%] flex-1 flex flex-col text-ssm bg-transparent">
 				<Footer />
 			</div>
 		</div>
---
+++ src/components/StoreSystemBox.tsx
@@ -0,0 +1,19 @@
+export default function StoreSystemBox() {
+	return (
+		<div className="m-auto w-[90%] 2xl:w-[75%] flex-1 flex flex-col text-ssm bg-transparent">
+			<div className="relative flex justify-between items-center px-6 py-4 w-full mx-auto z-20 bg-transparent ">
+				<div className="flex items-center gap-3">
+					<div className="text-[#1a73e8] text-xl">
+						<img src="/icons/map_end icon.svg" className="w-12 h-12" />
+					</div>
+					<p className="text-3xl font-medium text-gray-800">Xem hệ thống 88 cửa hàng trên toàn quốc</p>
+				</div>
+
+				<button className="relative px-6 py-3 bg-white rounded-full shadow-sm flex items-center gap-1">
+					<span className="text-2xl">Xem ngay</span>
+					<img src="/icons/arrow_end icon.svg" className="w-12 h-12" />
+				</button>
+			</div>
+		</div>
+	);
+}
